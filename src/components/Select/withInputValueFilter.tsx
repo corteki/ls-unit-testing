e2a0d2d0cd8a1f46defdf0889d60d2cfb7a32dcb
Tim Alaerts <tim.alaerts@capgemini.com>
2020-06-22 21:59:51 +0200

    Refined the types

--- src/components/Select/withInputValueFilter.tsx
+++ src/components/Select/withInputValueFilter.tsx
@@ -3,6 +3,9 @@ import { ListProps } from "../List/ListProps";
 
 export const withInputValueFilter = (Component: ReactType) => 
   (values: Array<string>, inputValue: string) => 
-  (props: Partial<ListProps>) => (
-    <Component {...props} values={values.filter(option => option.toLowerCase().includes(inputValue))}/>
+  (props: Pick<ListProps, 'isVisible' | 'onClick'>) => (
+    <Component 
+      {...props} 
+      values={values.filter(option => option.toLowerCase().includes(inputValue))}
+    />
 );
