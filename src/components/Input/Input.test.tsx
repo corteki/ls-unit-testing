de49c698f2c08231d78782666022276b92773753
Tim Alaerts <tim.alaerts@capgemini.com>
2020-07-06 13:44:15 +0200

    extracted input consts in separate file

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -2,25 +2,18 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import { Input } from "./Input";
+import { 
+  MOCK_HANDLE_INPUT, MOCK_HANDLE_CLEAR, MOCK_HANDLE_CLICK,
+  MOCK_DEFAULT_VALUE, MOCK_DEFAULT_PLACEHOLDER, MOCK_EMPTY_VALUE, MOCK_INPUT_EVENT
+} from './fixtures';
 
 describe('<Input/>', () => {
 
-  /**
-   * [1] mocking event handlers
-   */
-  const MOCK_HANDLE_INPUT = jest.fn();
-  const MOCK_HANDLE_CLEAR = jest.fn();
-  const MOCK_HANDLE_CLICK = jest.fn();
-
-  /**
-   * [2] instance is available via "screen",
-   * cleanup is done for us after each test
-   */
   beforeEach(() => {
     render(
       <Input 
-        value="MockValue" 
-        placeholder="MockPlaceholder"
+        value={MOCK_DEFAULT_VALUE} 
+        placeholder={MOCK_DEFAULT_PLACEHOLDER}
         onInput={MOCK_HANDLE_INPUT}
         onClear={MOCK_HANDLE_CLEAR}
         onClick={MOCK_HANDLE_CLICK}
@@ -28,15 +21,12 @@ describe('<Input/>', () => {
     );
   });
 
-  /**
-   * [3] verifying the default output of a input field
-   */
   it('should render an empty input field without a clear icon and with a down arrow icon', () => {
     const tree = renderer
       .create(
         <Input 
-          value="" 
-          placeholder="MockPlaceholder"
+          value={MOCK_EMPTY_VALUE} 
+          placeholder={MOCK_DEFAULT_PLACEHOLDER}
           onInput={MOCK_HANDLE_INPUT}
           onClear={MOCK_HANDLE_CLEAR}
           onClick={MOCK_HANDLE_CLICK}
@@ -46,22 +36,19 @@ describe('<Input/>', () => {
   });
 
   /**
-   * [4] '@testing-library/react' allows to avoid testing implementation details
+   * '@testing-library/react' allows to avoid testing implementation details
    */
   it('should render with a placeholder by default and render the value', () => {
     const input = screen.getByPlaceholderText(/MockPlaceholder/i);
-    /**
-     * [5] example of type specific matcher
-     */
-    expect(input).toHaveValue('MockValue');
+
+    expect(input).toHaveValue(MOCK_DEFAULT_VALUE);
   });
 
   it('should call the onInput handler when the input field is filled in', () => {
     const input = screen.getByPlaceholderText(/MockPlaceholder/i);
-    const mockEvent = { target: { value: 'MockInput' } };
 
     fireEvent.focus(input);
-    fireEvent.change(input, mockEvent);
+    fireEvent.change(input, MOCK_INPUT_EVENT);
 
     expect(MOCK_HANDLE_INPUT).toHaveBeenCalled();
   });
---
+++ src/components/Input/fixtures/index.ts
@@ -0,0 +1,8 @@
+export const MOCK_HANDLE_INPUT = jest.fn();
+export const MOCK_HANDLE_CLEAR = jest.fn();
+export const MOCK_HANDLE_CLICK = jest.fn();
+export const MOCK_EMPTY_VALUE = '';
+export const MOCK_DEFAULT_VALUE = 'MockValue';
+export const MOCK_DEFAULT_PLACEHOLDER = 'MockPlaceholder';
+export const MOCK_INPUT_VALUE = 'MockInput';
+export const MOCK_INPUT_EVENT = { target: { value: MOCK_INPUT_VALUE } };
