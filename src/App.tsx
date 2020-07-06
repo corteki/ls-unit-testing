639b8d02b8e07c84f2e2ae3ff4de47ce6dfc7a97
Tim Alaerts <tim.alaerts@capgemini.com>
2020-07-06 20:47:56 +0200

    changed the examples

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 import { Select, Users } from './components';
 import './App.scss';
 
+const MountainRanges = ['Andes', 'Southern Great Escarpment', 'Rocky Mountains', 'Transantarctic Mountains', 'Great Dividing Range'];
+const HighestPoints = ['Mount aconcagua', 'Thabana Ntlenyana', 'Mount Elbert', 'Mount Kirkpatrick', 'Mount Kosciuszko']
 function App() {
   return (
     <div className="App">
       <section className="App__container">
       <h1>With filter</h1>
         <Select
-          placeholder="Choose a biscuit you like"
-          options={['a', 'b']}
+          placeholder="Choose a mountain range"
+          options={MountainRanges}
         />
         <h1>Without filter</h1>
         <Select
           hasFilter={false}
-          placeholder="Choose a kitty to pet"
-          options={['c', 'd']}
+          placeholder="Choose a highest point"
+          options={HighestPoints}
         />
         <Users/>
       </section>
